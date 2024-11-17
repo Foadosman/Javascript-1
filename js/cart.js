5c8fba00134a4838fe70ddd38108151152828582
Foadosman <foad.osman95@gmail.com>
2024-11-17 14:07:04 +0100

    Finished

--- js/cart.js
+++ js/cart.js
@@ -13,7 +13,7 @@ function displayCart() {
         productElement.innerHTML = `
         <img src="${product.image}" alt="${product.title}">
         <h3>${product.title}</h3>
-        <p>price: ${product.discountedPrice  || product.price} NOK</p>
+        <p>price: ${product.discountedPrice  || product.price} $</p>
         <p>Size: ${product.selectedSize}</P>
         <button class="remove-button" data-index="${index}">remove</button> <!-- fjern-knapp -->
         `;
